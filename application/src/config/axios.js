9bcd0842f37cb8a9879abacc0f34e607da513ed9
xaobi <704123013@qq.com>
2017-08-24 18:22:55 +0800

    修改结构

--- application/src/config/axios.js
+++ application/src/config/axios.js
@@ -5,7 +5,7 @@
 var axios = require('axios')
 var config = require('./config')
 
-module.exports = axios.create({
+var instance = axios.create({
 	baseURL: config.default.dbURL,
 	/*withCredentials: true,//允许跨域请求发送cookie等用户认证凭据*/
 	dataType: 'JSONP',
@@ -13,3 +13,9 @@ module.exports = axios.create({
 		_session_token: config.default.token
 	}
 })
+
+/*添加拦截器
+instance.interceptors.request.use(function(){})
+ */
+
+module.exports = instance;
